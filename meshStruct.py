Enhance computeResidual method for Steger-Sorenson case by adding s_eta calculation, refining x_eta and y_eta computations, and correcting indexing for residual calculations.

meshStruct.py
@@ -110,7 +110,13 @@ class meshStruct:
         dx = np.zeros((self.jMax-2, self.kMax-2))
         dy = np.zeros((self.jMax-2, self.kMax-2))
 
+        currIter = 0
+
         while Res > self.params.convCriteria:
+
+            currIter += 1
+            #if currIter % 1000 == 0:
+            #    self.plotMesh()
 
             resx, resy, alpha, beta, gamma = self.computeResidual()
 
@@ -155,9 +161,9 @@ class meshStruct:
                 x_eta = (self.meshXs[2:, 1:-1] - self.meshXs[:-2, 1:-1]) / 2
                 y_eta = (self.meshYs[2:, 1:-1] - self.meshYs[:-2, 1:-1]) / 2
             case 'Steger-Sorenson':
+                x_eta = (self.meshXs[2:, 1:-1] - self.meshXs[:-2, 1:-1]) / 2
+                y_eta = (self.meshYs[2:, 1:-1] - self.meshYs[:-2, 1:-1]) / 2
                 s_eta = np.sqrt(x_eta**2 + y_eta**2)
-
-
 
         alpha = x_eta**2 + y_eta**2 
 
@@ -171,14 +177,33 @@ class meshStruct:
                 resy = (alpha * y_xixi + beta * y_xieta + gamma * y_ee)
 
             case 'Steger-Sorenson':
+                y_eta0 = np.zeros(self.jMax-2)
+                x_eta0 = np.zeros(self.jMax-2)
+                y_xi0 = np.zeros(self.kMax-2)
+                x_xi0 = np.zeros(self.kMax-2)
+
+                for eta in range(1, self.jMax-1):
+                    if eta < self.jMax - 1:
+                        y_eta0[eta-1] = (self.meshYs[eta+1, 0] - self.meshYs[eta-1, 0]) / 2
+                        x_eta0[eta-1] = (self.meshXs[eta+1, 0] - self.meshXs[eta-1, 0]) / 2
+                    else:
+                        y_eta0[eta-1] = (self.meshYs[-1, 0] - self.meshYs[eta-1, 0]) / 2
+                        x_eta0[eta-1] = (self.meshXs[-1, 0] - self.meshXs[eta-1, 0]) / 2
+
+                for eta in range(self.jMax):
+                    y_xi0 = self.ds * x_eta0 / np.sqrt(x_eta0**2 + y_eta0**2)
+                    x_xi0 = self.ds * y_eta0 / np.sqrt(x_eta0**2 + y_eta0**2)
+
+
+                
                 expa = np.exp(-self.etas[1:-1, 1:-1])
                 expb = np.exp(-self.etas[1:-1, 1:-1])
                 J = x_xi * y_eta - x_eta * y_xi
 
                 Rx = -J[:, 0] ** 2 * (alpha[:, 0] * x_xixi[:, 0] - 2 * beta[:, 0] * x_xieta[:, 0] + gamma[:, 0] * x_ee[:, 0])
                 Ry = -J[:, 0] ** 2 * (alpha[:, 0] * y_xixi[:, 0] - 2 * beta[:, 0] * y_xieta[:, 0] + gamma[:, 0] * y_ee[:, 0])
-                P0 = J[:, 0] * (y_eta[:, 0] * Rx - x_eta[:, 0] * Ry)
-                Q0 = J[:, 0] * (y_xi[:, 0] * Rx + x_xi[:, 0] * Ry)
+                P0 = J[:, 0] * (y_eta0 * Rx - x_eta0 * Ry)
+                Q0 = J[:, 0] * (y_xi0 * Rx + x_xi0 * Ry)
 
                 for i in range(0, self.kMax-2):
                     phi[i, :] = P0[i] * expa[i, :]
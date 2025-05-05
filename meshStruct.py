Fix residual calculation in computeResidual method for Steger-Sorenson case to correct x_ee and y_ee formulas.

meshStruct.py
@@ -116,7 +116,7 @@ class meshStruct:
         while Res > self.params.convCriteria:
 
             currIter += 1
-            if currIter % 1000 == 0:
+            if currIter % 2 == 0:
                 self.plotMesh()
 
             resx, resy, alpha, beta, gamma = self.computeResidual()
@@ -188,8 +188,8 @@ class meshStruct:
                 y_eta[:, 0] = np.sign(y_eta[:, 0]) * np.abs(self.ds * x_xi[:, 0] / np.sqrt(x_xi[:, 0]**2 + y_xi[:, 0]**2))
                 x_eta[:, 0] = np.sign(x_eta[:, 0]) * np.abs(self.ds * y_xi[:, 0] / np.sqrt(x_xi[:, 0]**2 + y_xi[:, 0]**2))
 
-                x_ee[:, 0] = 0.5 * (7 * self.meshXs[:, 0] + 8 * self.meshXs[:, 1] - self.meshXs[:, 2]) - 3 * x_eta[:, 0]
-                y_ee[:, 0] = 0.5 * (7 * self.meshYs[:, 0] + 8 * self.meshYs[:, 1] - self.meshYs[:, 2]) - 3 * y_eta[:, 0]
+                x_ee[:, 0] = 0.5 * (-7 * self.meshXs[:, 0] + 8 * self.meshXs[:, 1] - self.meshXs[:, 2]) - 3 * x_eta[:, 0]
+                y_ee[:, 0] = 0.5 * (-7 * self.meshYs[:, 0] + 8 * self.meshYs[:, 1] - self.meshYs[:, 2]) - 3 * y_eta[:, 0]
                 
 
         alpha = x_eta**2 + y_eta**2 
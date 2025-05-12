e99adc574f7393c05dc4bc17c86e2929121617f6
tague-carlyon <tague.carlyon@gmail.com>
2025-05-12 11:58:46 -0400

    Update airfoil parameters to NACA2410 and enhance mesh generation logic for improved accuracy

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from meshStruct import meshStruct
 from paramsStruct import paramsStruct
 
-params = paramsStruct.create(foil='NACA0010', gridGenType='Steger-Sorenson', stegerSorenOmega=0.02)
+params = paramsStruct.create(foil='NACA2410', gridGenType='Steger-Sorenson', stegerSorenOmega=0.02)
 mesh = meshStruct.create(params)
 mesh.assignInternalConditions()
 mesh.linearStretchMeshToWall()
--- meshStruct.py
+++ meshStruct.py
@@ -58,7 +58,47 @@ class meshStruct:
                 xint = 1.0
                 # get the NACA thickness
                 th = float(self.params.foil[-2:])/100
+
                 # y locations of for the top of the airfoil
+                ys[0, :] = 5 * th * (0.2969 * np.sqrt(xs * xint) - \
+                            0.1260 * xs * xint - 0.3516 * (xs * xint)**2 + \
+                            0.2843 * (xs * xint)**3 - 0.1015 * (xs * xint)**4)
+                # y locations of for the bottom of the airfoil
+                ys[1, :] = -ys[0, :]
+                
+                # assign bottom of the airfoil to the mesh
+                self.meshXs[:self.jLE-1, 0] = xs[1:][::-1]
+                self.meshYs[:self.jLE-1, 0] = ys[1, 1:][::-1]
+                # assign top of the airfoil to the mesh
+                self.meshXs[self.jLE:, 0] = xs[1:]
+                self.meshYs[self.jLE:, 0] = ys[0, 1:]
+                
+                # fix trailing edge of airfoil
+                self.meshYs[0, 0] = 0.0
+                self.meshYs[-1, 0] = 0.0
+                self.meshYs[1, 0] = 0.5*(self.meshYs[1, 0] + 0.25 * (self.meshYs[2, 0] + self.meshYs[0, 0]))
+                self.meshYs[-2, 0] = 0.5*(self.meshYs[self.jMax-2, 0] + 0.25 * (self.meshYs[self.jMax-1, 0] + self.meshYs[self.jMax-3, 0]))
+            
+            if 'NACA' in self.params.foil and self.params.foil[4:6] != '00':
+                # constant needed for NACA 4-digit airfoil generation
+                xint = 1.0
+                # get the NACA thickness
+                th = float(self.params.foil[-2:])/100
+                
+                maxCamb = float(self.params.foil[4])/100
+                positionMaxCamb = float(self.params.foil[5])/10
+
+                # Find the x indexes in xs that are closest to positionMaxCamb
+                idxMaxCamb = np.argmin(np.abs(xs - positionMaxCamb))
+                print(f"Index of maximum camber position: {idxMaxCamb}")
+
+                y_cFront = maxCamb / positionMaxCamb**2 * (2 * positionMaxCamb * xs - xs**2)
+                y_cBack = maxCamb / (1 - positionMaxCamb)**2 * (1 - 2 * positionMaxCamb + 2 * positionMaxCamb * xs - xs**2)
+
+                gradientFront = 2 * maxCamb / positionMaxCamb**2 * (positionMaxCamb - xs)
+                gradientBack = 2 * maxCamb / (1 - positionMaxCamb)**2 * (positionMaxCamb - xs)
+
+                                # y locations of for the top of the airfoil
                 ys[0, :] = 5 * th * (0.2969 * np.sqrt(xs * xint) - \
                             0.1260 * xs * xint - 0.3516 * (xs * xint)**2 + \
                             0.2843 * (xs * xint)**3 - 0.1015 * (xs * xint)**4)
